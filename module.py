880aa73fab98fffa0628dc87b566dc86716e1a02
Yakoa <123417941+Yakoa@users.noreply.github.com>
2024-07-04 18:15:46 -0400

    Create module.py

---
+++ module.py
@@ -0,0 +1,11 @@
+#This file is a demonstration of Python Modules - Go to section Python Modules on line 1471
+
+#Notice how you can put anything in this file?
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
